c6281b5b84999ceaa4d29a2923be36019711c0a5
naachoperez1 <85687161+naachoperez1@users.noreply.github.com>
2023-03-29 19:28:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import requests
+import os
+from twilio.rest import Client
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
+############################# STOCKS GETTER #############################
+EMPRESA = 'microsoft'
+STOCK = "MSFT"
+API_KEY_STOCKS = 'MHQUYP5W2JBFWY7I'
+ENDPOINT_STOCKS = 'https://www.alphavantage.co/query'
+API_KEY_NEWS = '7536a63cb0924832887fee383e81d506'
+ENDPOINT_NEWS = 'https://newsapi.org/v2/everything'
+
+
+parametros_stocks = {
+    'function' : 'TIME_SERIES_DAILY_ADJUSTED',
+    'symbol' : STOCK,
+    'outputsize':'compact',
+    'apikey' : API_KEY_STOCKS,
+}
+
+parametros_news = {
+    'q': EMPRESA,
+    'from': '2023-02-20',
+    'sortBy': 'popularity',
+    'apiKey': API_KEY_NEWS
+}
+
+peticion = requests.get(ENDPOINT_STOCKS, params = parametros_stocks).json()
+peticion_news = requests.get(ENDPOINT_NEWS, params=parametros_news).json()
+
+# Convierto el json a una lista para obtener los dos primeros valores (los dos ultimos dias habiles)
+lista_dias = list(peticion['Time Series (Daily)'].items())
+fecha_hoy = lista_dias[0][0]
+fecha_ayer = lista_dias[1][0]
+
+stock_hoy = peticion['Time Series (Daily)'][fecha_hoy]
+stock_ayer = peticion['Time Series (Daily)'][fecha_ayer]
+close_price_hoy = stock_hoy['4. close']
+close_price_ayer = stock_ayer['4. close']
+
+print(f"{STOCK} al {fecha_hoy}:{stock_hoy}")
+print(f"{STOCK} al {fecha_ayer}:{stock_ayer}")
+
+# Chequeo si el close_price de hoy fue menor o mayor al close_price de ayer
+diferencia = float(close_price_hoy) - float(close_price_ayer)
+
+def get_diff():
+    if diferencia>0:
+        print(f"+${diferencia}")
+        return f"+${diferencia}"
+    elif diferencia<0:
+        print(f"${diferencia}")
+        return f"${diferencia}"
+    else:
+        print(f"${diferencia}")
+        return f'$={diferencia}'
+
+
+################################## NEWS GETTER ##################################
+lista = peticion_news['articles']
+titulares = [noticia['title'] for noticia in lista[:3]]
+noticias = [noticia['description'] for noticia in lista[:3]]
+# for i in range(3):
+#     print(f"{titulares[i]}\n {noticias[i]}")
+
+################################## SMS ##################################
+account_sid = "AC9034b05209dd0d21330a032ffcd2cf64"
+auth_token = 'c5f1bba9b4fd4876c269cd2d100634a0'
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+      body=f"{STOCK}:{get_diff()}.\n{titulares[0]}.\n {noticias[0]}.\n{titulares[1]}."
+           f"\n{noticias[1]}.\n{titulares[2]}.\n{noticias[2]}.",
+      from_="+15076667886",
+      to="+543564647541"
+)
